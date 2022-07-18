8d2839c924034847326beb85e315b4937a97df88
lfelipibarone <lfelipibarone@gmail.com>
2022-07-18 13:10:46 -0300

    new

--- Fundamentos C++/Condicionais Matemáticas/condicionalmatematica.c
+++ Fundamentos C++/Condicionais Matemáticas/condicionalmatematica.c
@@ -5,7 +5,39 @@
 
 void main()
 {
+    // Definindo Variaveis
+    int a = 5, b = 10, c = 15;
+    char  d = 'x';
 
+    // Maior
+    if ( a > 2){
+        printf("\n %d e maior que 2  \n", a);
+    }
+    // Maior ou Igual
+    if ( c>= b){
+        printf("\n %d e maior ou igual %d  \n", c, b);
+    }
+
+    //Menor
+    if ( a < 10){
+        printf("\n %d e menor que 10  \n", a);
+    }
+
+    // Menor ou Igual
+    if ( a<= 10){
+        printf("\n %d e menor ou igual a 10 \n", a);
+    }
+
+    //Diferente
+
+    if( c!= 10){
+        printf("\n %d nao e 10 \n", c);
+    }
+
+    if ( d!= 'a'){
+        printf("\n %c nao e a \n", d);
+
+    }
 
     //Finalização do Sistema
     system ("pause");
---
+++ Fundamentos C++/Condicionais Múltiplas e Conectivos/condicionaismec.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stdbool.h>
+
+void main()
+{
+
+
+    //Finalização do Sistema
+    system ("pause");
+    return 0;
+
+}
